400161ee4466f5a67159daab51a11358bdabe760
mikewng <wangzhxn0@gmail.com>
2026-05-05 21:45:56 -0400

    updated to contracts

--- Core/Services/Contracts/INoteGraphService.cs
+++ Core/Services/Contracts/INoteGraphService.cs
@@ -1,7 +1,11 @@
-﻿namespace graphnotelm.Core.Services.Contracts
+﻿using graphnotelm.Utils;
+
+namespace graphnotelm.Core.Services.Contracts
 {
     public interface INoteGraphService
     {
+        public Result<bool> GetNoteGraphById(Guid noteGraphId);
+        public Result<bool> GetNote(Guid noteGraphId);
 
     }
 }
