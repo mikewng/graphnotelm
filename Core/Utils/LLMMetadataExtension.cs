688beb54c965142f6684bf1ab0affce48e0135cf
mikewng <wangzhxn0@gmail.com>
2026-05-13 02:06:17 -0400

    Create LLMMetadataExtension.cs

---
+++ Core/Utils/LLMMetadataExtension.cs
@@ -0,0 +1,29 @@
+﻿using graphnotelm.Core.Models;
+using System.Text.Json;
+
+namespace graphnotelm.Core.Utils
+{
+    public static class LLMMetadataExtension
+    {
+        public static T? GetNamespace<T>(this NoteNodeMetadata metadata, string ns) where T : class
+        {
+            try
+            {
+                var document = JsonDocument.Parse(metadata.LLMMetadata);
+                if (document.RootElement.TryGetProperty(ns, out var section))
+                {
+                    return JsonSerializer.Deserialize<T>(section.GetRawText());
+                }
+            }
+            catch { }
+            return null;
+        }
+
+        public static void SetNamespace<T>(this NoteNodeMetadata metadata, string ns, T value)
+        {
+            var dict = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(metadata.LLMMetadata) ?? new();
+            dict[ns] = JsonSerializer.SerializeToElement(value);
+            metadata.LLMMetadata = JsonSerializer.Serialize(dict);
+        }
+    }
+}
