aa69a8bad0ce670f2d0cdb205b912e618bf6d9e6
mikewng <wangzhxn0@gmail.com>
2026-05-16 16:05:31 -0400

    graph context to eliminate reabsorbing graph?

---
+++ Core/Utils/Tools/GraphContentTools.cs
@@ -0,0 +1,26 @@
+﻿using graphnotelm.Core.Models;
+using graphnotelm.Core.Services.Contracts;
+using System.ComponentModel;
+
+namespace graphnotelm.Core.Utils.Tools
+{
+    public class GraphContentTools
+    {
+        private readonly INoteNodeService _nodeService;
+        public GraphContentTools(INoteNodeService noteNodeService)
+        {
+            _nodeService = noteNodeService;
+        }
+
+        [Description("Gets a single node's full content based off node ID. This includes the title, confidence score, note content, and relationships")]
+        public NoteNode GetNodeById(
+            [Description("The graph note ID based off of type Guid in C#.")]
+            Guid graphNoteId,
+            [Description("The node ID based off of type Guid in C#.")]
+            Guid noteNodeId
+            )
+        {
+            return new NoteNode();
+        }
+    }
+}
