35c4c9e58d1acd8d94d3bd0ceff1c3e6bfdc46e8
mikewng <wangzhxn0@gmail.com>
2026-05-20 14:55:13 -0400

    Adding SQLite stub for executeable

---
+++ Infrastructure/Repository/SQLiteNoteGraphMetadataRepository.cs
@@ -0,0 +1,31 @@
+﻿using graphnotelm.Core.Models;
+using graphnotelm.Infrastructure.Repositories;
+using graphnotelm.Infrastructure.Repository.Contracts;
+
+namespace graphnotelm.Infrastructure.Repository
+{
+    public class SQLiteNoteGraphMetadataRepository : Repository<NoteGraphMetadata>, INoteGraphMetadataRepository
+    {
+        public SQLiteNoteGraphMetadataRepository(AppDbContext context) : base(context) { }
+
+        public Task AddAsync(NoteGraphMetadata noteGraphMetadata, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NoteGraphMetadata?> GetByIdAsync(Guid noteGraphId, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<NoteGraphMetadata>> GetListByUserIdAsync(Guid userId, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(NoteGraphMetadata noteGraphMetadata, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
